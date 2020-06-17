184e69a84946c113e0aade6b4b0ab9b4c644672e
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2020-06-17 12:34:02 +0700

    Change splash screen
    Former-commit-id: 8079419983d176fec7c90ab6f4b8122b49c80e44

--- frontend/android/app/src/main/java/cf/contrasleuth/MainActivity.kt
+++ frontend/android/app/src/main/java/cf/contrasleuth/MainActivity.kt
@@ -1,43 +1,106 @@
 package cf.contrasleuth
 
+import android.Manifest
+import android.annotation.TargetApi
+import android.content.BroadcastReceiver
 import android.content.Context
-import android.os.Build
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.LocalBroadcastManager
 import android.util.Log
-import com.getcapacitor.BridgeActivity
-import com.getcapacitor.Plugin
-import java.io.*
+import com.getcapacitor.*
+import org.json.JSONObject
 import java.util.*
 
-class MainActivity : BridgeActivity() {
-    @Throws(IOException::class)
-    fun getCacheFile(context: Context, filename: String): File = File(context.cacheDir, filename)
-            .also {
-                it.outputStream().use { cache -> context.assets.open(filename).use { it.copyTo(cache) } }
+@NativePlugin
+class EchoPlugin : Plugin() {
+    override fun load() {
+        val localBroadcastManager = LocalBroadcastManager.getInstance(context)
+
+        val receiver: BroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val line = intent.getStringExtra("line")
+                val json = JSONObject()
+                json.put("line", line)
+                bridge.triggerWindowJSEvent("stdout line", json.toString())
             }
+        }
+
+        localBroadcastManager.registerReceiver(receiver, IntentFilter("stdout line"))
+    }
+
+    @PluginMethod
+    fun send(call: PluginCall) {
+        val localBroadcastManager = LocalBroadcastManager.getInstance(context)
+
+        val line = call.getString("line")
+        val ret = JSObject()
+
+        val intent = Intent("stdin line")
+        intent.putExtra("line", line)
+        localBroadcastManager.sendBroadcast(intent)
+
+        call.success(ret)
+    }
+}
+
+class MainActivity : BridgeActivity() {
+    private val locationRequestCode = 2020
+
+    @TargetApi(26)
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        val localBroadcastManager = LocalBroadcastManager.getInstance(this)
+        when (requestCode) {
+            locationRequestCode -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    this.finishAffinity()
+                } else {
+                    val intent = Intent(this, MyService::class.java)
+                    this.startService(intent)
+
+                    Timer().scheduleAtFixedRate(object : TimerTask() {
+                        override fun run() {
+                            run {
+                                val json = JSONObject()
+                                val nested = JSONObject()
+                                nested.put("address", "192.168.43.96:4016")
+                                nested.put("operation_id", UUID.randomUUID().toString())
+                                json.put("EstablishConnection", nested)
+                                val intent = Intent("stdin line")
+                                intent.putExtra("line", Base64.getEncoder().encodeToString(json.toString().toByteArray()) + "\n")
+                                localBroadcastManager.sendBroadcast(intent)
+                            }
+
+                            run {
+                                val json = JSONObject()
+                                val nested = JSONObject()
+                                nested.put("address", "192.168.43.96:4017")
+                                nested.put("operation_id", UUID.randomUUID().toString())
+                                json.put("EstablishReverseConnection", nested)
+                                val intent = Intent("stdin line")
+                                intent.putExtra("line", Base64.getEncoder().encodeToString(json.toString().toByteArray()) + "\n")
+                                localBroadcastManager.sendBroadcast(intent)
+                            }
+                        }
+                    }, 0, 5000)
+                }
+            }
+        }
+    }
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val executable = when (Build.SUPPORTED_ABIS[0]) {
-            "arm64-v8a" -> getCacheFile(this, "contrasleuth-aarch64-linux-android")
-            "armeabi" -> getCacheFile(this, "contrasleuth-arm-linux-androideabi")
-            "armeabi-v7a" -> getCacheFile(this, "contrasleuth-armv7-linux-androideabi")
-            "x86" -> getCacheFile(this, "contrasleuth-i686-linux-android")
-            "x86_64" -> getCacheFile(this, "contrasleuth-x86_64-linux-androideabi")
-            else -> getCacheFile(this, "contrasleuth-arm-linux-androideabi")
-        }
-        executable.setExecutable(true)
-        val sh = Runtime.getRuntime().exec(executable.path + " --help")
-        val outputStream = DataOutputStream(sh.outputStream)
-        val bufferedInputStream = BufferedReader(InputStreamReader(sh.inputStream))
-        while (true) {
-            val line = bufferedInputStream.readLine() ?: break
-            Log.e("TAG", line)
-        }
-
-        // Initializes the Bridge
         init(savedInstanceState, object : ArrayList<Class<out Plugin?>?>() {
+            init {
+                add(EchoPlugin::class.java)
+            }
         })
+
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), locationRequestCode)
     }
 }
