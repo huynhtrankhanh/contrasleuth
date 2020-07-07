f2d05eb0712936ffb7d6b6d27732515da5a27dc5
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2020-07-07 20:59:59 +0700

    Initial work on the QUIC tunnel

--- frontend/android/app/src/main/java/app/contrasleuth/MyService.kt
+++ frontend/android/app/src/main/java/app/contrasleuth/MyService.kt
@@ -1,5 +1,6 @@
 package app.contrasleuth
 
+import android.Manifest
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -8,32 +9,130 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
 import android.net.wifi.p2p.WifiP2pManager
-import android.net.wifi.p2p.WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo
 import android.os.Build
 import android.os.IBinder
-import android.os.Parcelable
-import android.annotation.TargetApi
+import android.os.Looper
 import android.util.Log
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import java.io.BufferedReader
 import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
-import org.json.JSONObject
-import java.util.*
 
 // https://stackoverflow.com/questions/47531742
 class MyService : Service() {
+    val TAG = "TAG"
+
     @Throws(IOException::class)
     fun getCacheFile(context: Context, filename: String): File = File(context.cacheDir, filename)
             .also {
                 it.outputStream().use { cache -> context.assets.open(filename).use { it.copyTo(cache) } }
             }
 
+    // Adapted from https://android.googlesource.com/platform/development/+/master/samples/WiFiDirectDemo/src/com/example/android/wifidirect/WiFiDirectActivity.java
+    fun establishP2p() {
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_DIRECT)) {
+            Log.e(TAG, "Wi-Fi Direct is not supported by this device.")
+            return
+        }
+
+        val appContext = getApplicationContext()
+
+        val wifiManager = appContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        if (wifiManager == null) {
+            Log.e(TAG, "Cannot get Wi-Fi system service.")
+            return
+        }
+
+        if (!wifiManager.isP2pSupported()) {
+            Log.e(TAG, "Wi-Fi Direct is not supported by the hardware or Wi-Fi is off.")
+            return
+        }
+
+        val manager = appContext.getSystemService(Context.WIFI_P2P_SERVICE) as WifiP2pManager
+
+        if (manager == null) {
+            Log.e(TAG, "Cannot get Wi-Fi Direct system service.")
+            return
+        }
+
+        val channel = manager.initialize(this, Looper.getMainLooper(), null)
+
+        if (channel == null) {
+            Log.e(TAG, "Cannot initialize Wi-Fi Direct.")
+            return
+        }
+
+        val intentFilter = IntentFilter().apply {
+            addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION)
+            addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION)
+        }
+
+        // Adapted from https://developer.android.com/guide/topics/connectivity/wifip2p
+        registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val action: String = intent.action!!
+                when (action) {
+                    WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                        // Check to see if Wi-Fi is enabled and notify appropriate activity.
+                    }
+                    WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                        // Call WifiP2pManager.requestPeers() to get a list of current peers.
+                    }
+                    WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                        // Respond to new connection or disconnections.
+                    }
+                    WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                        // Respond to this device's wifi state changing.
+                    }
+                }
+            }
+        }, intentFilter)
+
+        // Clear local services before broadcasting: https://stackoverflow.com/a/31641302
+        manager.clearLocalServices(channel, object : WifiP2pManager.ActionListener {
+            override fun onSuccess() {
+                // ACCESS_FINE_LOCATION permission was already requested in main.
+                if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return
+                }
+
+                val record = mapOf("key" to "value")
+                val info = WifiP2pDnsSdServiceInfo.newInstance(
+                        "_quic-tunnel._contrasleuth-mvp",
+                        "_quic-tunnel._contrasleuth-mvp",
+                        record
+                )
+
+                manager.addLocalService(channel, info, object : WifiP2pManager.ActionListener {
+                    override fun onSuccess() {
+                        // Handle success.
+                    }
+
+                    override fun onFailure(reason: Int) {
+                        // Handle failure.
+                    }
+                })
+            }
+
+            override fun onFailure(reason: Int) {
+                // Handle failure.
+            }
+        })
+    }
+
     override fun onCreate() {
+        establishP2p();
+
         val channelId =
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                     createNotificationChannel("my_service", "Contrasleuth")
