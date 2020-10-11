acbbc43556f58b16fb76fb0e7b6947411dba7022
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2020-10-11 09:02:58 +0000

    Compile Quinn for Android

--- frontend/abridged-assemble.sh
+++ frontend/abridged-assemble.sh
@@ -4,4 +4,7 @@ cd ../backend
 cross build --target=aarch64-linux-android --release
 mkdir -p ../frontend/android/app/src/main/jniLibs/arm64-v8a/
 mv ./target/aarch64-linux-android/release/contrasleuth ../frontend/android/app/src/main/jniLibs/arm64-v8a/libcontrasleuth.so
+cd ../quinn
+cross build --target=aarch64-linux-android --release
+mv ./target/aarch64-linux-android/release/quinn ../frontend/android/app/src/main/jniLibs/arm64-v8a/libquinn.so
 cd ../frontend
