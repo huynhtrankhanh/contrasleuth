169b1dcb404f6ba2668546e27473493364311723
Huỳnh Trần Khanh <qcdz9r6wpcbh59@gmail.com>
2020-10-11 07:21:48 +0000

    Move Contrasleuth binaries to jniLibs folder (https://stackoverflow.com/questions/24357687/how-to-include-so-library-in-android-studio)

--- frontend/assemble.sh
+++ frontend/assemble.sh
@@ -6,9 +6,16 @@ cross build --target=arm-linux-androideabi --release
 cross build --target=armv7-linux-androideabi --release
 cross build --target=i686-linux-android --release
 cross build --target=x86_64-linux-android --release
-mv ./target/aarch64-linux-android/release/contrasleuth ../frontend/android/app/src/main/assets/contrasleuth-aarch64-linux-android
-mv ./target/arm-linux-androideabi/release/contrasleuth ../frontend/android/app/src/main/assets/contrasleuth-arm-linux-androideabi
-mv ./target/armv7-linux-androideabi/release/contrasleuth ../frontend/android/app/src/main/assets/contrasleuth-armv7-linux-androideabi
-mv ./target/i686-linux-android/release/contrasleuth ../frontend/android/app/src/main/assets/contrasleuth-i686-linux-android
-mv ./target/x86_64-linux-android/release/contrasleuth ../frontend/android/app/src/main/assets/contrasleuth-x86_64-linux-android
+
+mkdir -p ../frontend/android/app/src/main/jniLibs/arm64-v8a
+mkdir -p ../frontend/android/app/src/main/jniLibs/armeabi
+mkdir -p ../frontend/android/app/src/main/jniLibs/armeabi-v7a
+mkdir -p ../frontend/android/app/src/main/jniLibs/x86
+mkdir -p ../frontend/android/app/src/main/jniLibs/x86_64
+
+mv ./target/aarch64-linux-android/release/contrasleuth ../frontend/android/app/src/main/jniLibs/arm64-v8a/libcontrasleuth.so
+mv ./target/arm-linux-androideabi/release/contrasleuth ../frontend/android/app/src/main/jniLibs/armeabi/libcontrasleuth.so
+mv ./target/armv7-linux-androideabi/release/contrasleuth ../frontend/android/app/src/main/jniLibs/armeabi-v7a/libcontrasleuth.so
+mv ./target/i686-linux-android/release/contrasleuth ../frontend/android/app/src/main/jniLibs/x86/libcontrasleuth.so
+mv ./target/x86_64-linux-android/release/contrasleuth ../frontend/android/app/src/main/jniLibs/x86_64/libcontrasleuth.so
 cd ../frontend
